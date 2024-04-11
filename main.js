33afe886662469e5437b02b850c85dd63f3a1a7a
Ben <bengeraghty.bg@gmail.com>
2024-04-11 13:05:30 +0700

    add timer function to countdown

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('Testing line for JavaScript');
+const dayNumber = document.getElementById('days');
+const hourNumber = document.getElementById('hours');
+const minuteNumber = document.getElementById('minutes');
+const secondNumber = document.getElementById('seconds');
+
+
+const targetDate = new Date('2025-11-04T00:00:00');
+
+// COUNTDOWN TIMER
+function countdown() {
+    
+    const now = new Date().getTime();
+    const difference = targetDate - now;    
+
+const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+
+dayNumber.textContent = days;
+hourNumber.textContent = hours;
+minuteNumber.textContent = minutes;
+secondNumber.textContent = seconds;
+
+}
+
+// Update timer
+const timerUpdate = setInterval(countdown, 1000);
+
+
+countdown();
