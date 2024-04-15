303263ee1c024f7c69ac9b456bc2e002d154ba70
Ben <bengeraghty.bg@gmail.com>
2024-04-15 10:34:43 +0700

    add basic form structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import htmlPurge from 'vite-plugin-purgecss'
+import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
+import { resolve } from 'path';
 
-export default {
-    plugins: [
-        htmlPurge(),
-    ]
-}
+export default defineConfig({
+  plugins: [
+    htmlPurge(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        // Define your entry points here
+        index: resolve(__dirname, 'src/index.html'),
+        signup: resolve(__dirname, 'src/signup.html'),
+      }
+    }
+  }
+});
