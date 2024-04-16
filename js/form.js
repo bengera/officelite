fbe182b01324091f3d57bdd591d8bb7a85c69a8d
Ben <bengeraghty.bg@gmail.com>
2024-04-16 14:41:17 +0700

    add validation for form

--- js/form.js
+++ js/form.js
@@ -39,10 +39,40 @@ function checkInputs() {
 
     } else {
         console.log('Success')  
-        setSuccessFor(name)
+        setSuccessFor(name);
     }
 
-    console.log(nameValue,emailValue,phoneNumberValue, companyValue);
+    if (emailValue === '') {
+        console.log('Field cannot be empty') 
+        setErrorFor(email);
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email);
+        console.log('Invalid email')
+
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (phoneNumberValue === '') {
+        console.log('Field cannot be empty') 
+        setErrorFor(phoneNumber);
+    } else if (!isPhoneNumber(phoneNumberValue)) {
+        setErrorFor(phoneNumber);
+        console.log('Invalid phone number')
+
+    } else {
+        setSuccessFor(phoneNumber);
+    }
+
+    if (companyValue === '') {
+        console.log('Field cannot be empty') 
+        setErrorFor(company);
+
+    } else {
+        console.log('Success')  
+        setSuccessFor(company);
+    }
+
 }
 
 function setSuccessFor(input) {
@@ -56,4 +86,12 @@ function setErrorFor(input) {
     const formControl = input.parentElement;
     const errorIcon = formControl.querySelector('.i-error');
     errorIcon.setAttribute("style", "display:block");
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function isPhoneNumber(phoneNumber) {
+    return /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/.test(phoneNumber);
 }
